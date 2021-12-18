59da634f273936d7398c41c83f754ebfa1873e97
FieryCake <glenn-ham@hotmail.com>
2021-12-19 02:56:16 +0800

    changes to routes
    greyed out 3rd line, 13th line and added proper api key

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router(); 
-var test = require('../static/data')
+// var test = require('../static/data')
 // var test = "Lol"
-setTimeout(function() {console.log(test.data)}, 4000);
+// setTimeout(function() {console.log(test.data)}, 4000);
 
 
 
@@ -10,15 +10,18 @@ setTimeout(function() {console.log(test.data)}, 4000);
 /* GET home page. */
 // setTimeout(function() {console.log(test.data)}, 4000);
 
-router.get('/', function(req, res, next) {
-  console.log(typeof(data))
-  setTimeout(function() {res.render('index.html',{data:test,datalel:JSON.stringify(test.data),lol:"Glenn"} );}, 4000);
-  
-});
 // router.get('/', function(req, res, next) {
-//   setTimeout(function() {res.render('index.html',{data:test,lol:"Glenn"} ); }, 1000);
+//   console.log(typeof(data))
+//   setTimeout(function() {res.render('index.html',{data:test,datalel:JSON.stringify(test.data),lol:"Glenn"} );}, 4000);
   
 // });
+
+router.get('/', function(req, res, next) {
+  console.log(typeof(data))
+  setTimeout(function() {res.render('index.html',{lol:"Glenn"} );}, 4000);
+  
+});
+
 
 router.get('/c', function(req, res, next) {
   res.render('contact.html');
